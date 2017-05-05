c30aa01f06c58b47f332225b29a16a387c979a3c
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-05-05 18:25:24 +0200

    Fix cmd-click

--- src/Events.elm
+++ src/Events.elm
@@ -1,20 +1,42 @@
 module Events exposing (..)
 
 import Html
-import Html.Events exposing (on, onWithOptions, targetValue)
-import Json.Decode as Json
-
-
-preventDefaultOptions : Html.Events.Options
-preventDefaultOptions =
-    { preventDefault = True, stopPropagation = True }
+import Html.Events exposing (on, onWithOptions, targetValue, defaultOptions)
+import Json.Decode
 
 
 onChange : (String -> msg) -> Html.Attribute msg
 onChange msg =
-    on "change" (Json.map msg targetValue)
+    on "change" (Json.Decode.map msg targetValue)
 
 
 onClickPreventDefault : msg -> Html.Attribute msg
-onClickPreventDefault msg =
-    onWithOptions "click" preventDefaultOptions (Json.succeed msg)
+onClickPreventDefault message =
+    onWithOptions "click"
+        { defaultOptions | preventDefault = True }
+        (preventDefault2
+            |> Json.Decode.andThen (maybePreventDefault message)
+        )
+
+
+preventDefault2 : Json.Decode.Decoder Bool
+preventDefault2 =
+    Json.Decode.map2
+        (invertedOr)
+        (Json.Decode.field "ctrlKey" Json.Decode.bool)
+        (Json.Decode.field "metaKey" Json.Decode.bool)
+
+
+maybePreventDefault : msg -> Bool -> Json.Decode.Decoder msg
+maybePreventDefault msg preventDefault =
+    case preventDefault of
+        True ->
+            Json.Decode.succeed msg
+
+        False ->
+            Json.Decode.fail "Normal link"
+
+
+invertedOr : Bool -> Bool -> Bool
+invertedOr x y =
+    not (x || y)
