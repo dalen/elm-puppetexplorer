7f51cd444085cd0ff045091c1375fb20d443e60b
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-06-09 13:32:24 +0200

    Remove unused FA spinner

--- src/View/Spinner.elm
+++
@@ -1,9 +0,0 @@
-module View.Spinner exposing (view)
-
-import Html
-import Html.Attributes as Attributes
-
-
-view : Html.Html msg
-view =
-    Html.i [ Attributes.class "fa fa-spinner fa-spin" ] []
